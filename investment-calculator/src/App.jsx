a90ac50cf80e5e8c5cef49d3af9c2b63950d1646
adityagit144 <145044058+adityagit144@users.noreply.github.com>
2025-10-01 11:22:06 +0530

    App.jsx

--- investment-calculator/src/App.jsx
+++ investment-calculator/src/App.jsx
@@ -11,6 +11,7 @@ function App() {
     duration: 10,
   });
 
+  const inputIsValid = userInput.duration >= 1;
   function handleChange(inputIdentifier, newValue) {
     setUserInput((prevUserInput) => {
       return {
@@ -24,7 +25,10 @@ function App() {
     <>
       <Header />
       <UserInput userInput={userInput} onChange={handleChange} />
-      <Results input={userInput} />
+      {!inputIsValid && (
+        <p className="center">please enter a duration greater than zero.</p>
+      )}
+      {inputIsValid && <Results input={userInput} />}
     </>
   );
 }
